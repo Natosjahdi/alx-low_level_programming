b111ba44ac1decb792c0859900cf620f10865879
Natosjahdi <natosjahdi@gmail.com>
2023-06-15 13:20:59 -0700

    printing names

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+/**
+ * Description: Prints "Programming is like building a multilingual puzzle".
+ *
+ * Return: 0 (Success)
+  */
 int main(void)
 {
 char *message = "\"Programming is like building a multilingual puzzle";
 puts(message);
-puts("");
 return 0;
 }
 
