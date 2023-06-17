de7586ca5d47af4ceebfcd548463a930285763f5
Natosjahdi <natosjahdi@gmail.com>
2023-06-16 18:52:57 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (indicating successful execution)
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return 0;
+}
