e3e447ce4d10ae4eb17b036a258f13f6c27b3d0e
Natosjahdi <natosjahdi@gmail.com>
2023-06-16 22:37:31 -0700

    only single

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - printing only possible digits
+* Return: Always 0 (indicating successful execution)
+*/
+int main(void) {
+int number;
+for (number = 0; number < 10; number++)
+{
+putchar(number + '0');
+if (number != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
