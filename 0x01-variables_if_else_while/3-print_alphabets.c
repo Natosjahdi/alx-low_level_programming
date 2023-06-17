cd84203d1097365dd8ccc0d71771756373fb766c
Natosjahdi <natosjahdi@gmail.com>
2023-06-16 21:16:52 -0700

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (indicating successful execution)
+*/
+int main(void)
+{
+char alphabet, uppercase;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet\n);
+}
+putchar('\n');
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
+putchar(uppercase);
+}
+putchar('\n');
+return (0);
+}
