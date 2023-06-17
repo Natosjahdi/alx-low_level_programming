9d73fd88a55d6920df3bf26c3a99522618ab2b78
Natosjahdi <natosjahdi@gmail.com>
2023-06-16 22:07:44 -0700

    single

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,10 +6,10 @@
 int main(void)
 {
 int number;
-for (number = 0; number < 10; number++) 
+for (number = 0; number < 10; number++)
 {
 putchar(number + '0');
 }
 putchar('\n');
-return 0;
+return (0);
 }
