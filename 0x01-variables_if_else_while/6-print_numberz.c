a24d176a023980686d30319a7842d5b6f79b81b9
Natosjahdi <natosjahdi@gmail.com>
2023-06-16 22:06:16 -0700

    single

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - print the letter and != some letters
+* Return: Always 0 (indicating successful execution)
+*/
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++) 
+{
+putchar(number + '0');
+}
+putchar('\n');
+return 0;
+}
