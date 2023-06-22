eb1007304fab1084495d0bd70f1e95d8af21c56f
Natosjahdi <natosjahdi@gmail.com>
2023-06-22 16:51:04 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* jack_bauer - print every minute of a days
+* Description: print minutes 00:00 to 23:59
+*/
+
+void jack_bauer(void)
+{
+int hours, min;
+
+for (hours = 0; hours < 24; hours++)
+{
+for (min = 0; min < 60; min++)
+{
+if (hours < 10)
+{
+_putchar('0' + hours);
+}
+else
+{
+_putchar('0' + (hours / 10));
+_putchar('0' + (hours % 10));
+}
+_putchar(':');
+if (min < 10)
+{
+_putchar('0' + min);
+}
+else
+{
+_putchar('0' + (min / 10));
+_putchar('0' + (min % 10));
+}
+_putchar('\n');
+}
+}
+}
