1264d82f9276fb47ee96ceef2eb5e70f67fed382
Natosjahdi <natosjahdi@gmail.com>
2023-06-22 15:29:26 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - value of an integer.
+* Return: The absolute value of n.
+*/
+int print_last_digit(int)
+{
+int num;
+int lastDigit = num % 10;
+_putchar(lastDigit + '0');
+return (lastDigit);
+}i
