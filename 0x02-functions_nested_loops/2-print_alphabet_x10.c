9446bd82801e0843a2d3801ed94bb26dc12d03f3
Natosjahdi <natosjahdi@gmail.com>
2023-06-22 04:48:07 -0700

    lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
-* print_alpahabet_x10 - Prints the alphabet 10 times.
-*
-* description:print_alpahabet_x10 Write a function that prints 10 times the.
+* print_alphabet_x10 - Prints the alphabet 10 times.
 *
 * Return: Always 0 (success)
 */
