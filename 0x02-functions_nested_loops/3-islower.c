7b2eae14af5b5e6686b294b606d7909fca228ab9
Natosjahdi <natosjahdi@gmail.com>
2023-06-22 04:22:44 -0700

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
 * int _islower(int c)- Prints the alphabet lower case times.
-* 
 * description:Write a function that prints 10 times the alphab.
-*
 * Return: Always 0 (success)
 */
 int _islower(int c)
